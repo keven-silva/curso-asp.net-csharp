132595a6d56c032ed93d6e27aaa9493f1b89cf5f
Keven <56522524+Keven-Almeida@users.noreply.github.com>
2022-02-09 23:18:41 -0300

    Reiniciado projeto SalesWebMvc

--- SaleWebMvc/SaleWebMvc/Models/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SaleWebMvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
